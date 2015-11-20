ecdd70cef74096e9e8c52ee300054727009e100f
Hudo Assenco <hudo.assenco@gmail.com>
2015-11-19 19:25:22 -0800

    finishing screen

--- app/src/main/java/com/assignment/sjsu/hudoassenco/cmpe137lab2/MainActivity.java
+++ app/src/main/java/com/assignment/sjsu/hudoassenco/cmpe137lab2/MainActivity.java
@@ -1,15 +1,19 @@
 package com.assignment.sjsu.hudoassenco.cmpe137lab2;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner _spinner;
+    Button _calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +22,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        _spinner = (Spinner) findViewById(R.id.term_input);
+        _calculate = (Button) findViewById(R.id.calculate_bt);
+
+        _calculate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                calculate();
             }
         });
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.terms_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        _spinner.setAdapter(adapter);
     }
 
     @Override
@@ -43,10 +56,20 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_reset) {
+            cleanForm();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void calculate() {
+        //TODO: Execute and Output Calculation
+    }
+
+    private void cleanForm() {
+        //TODO: Clean the form
+
     }
 }
